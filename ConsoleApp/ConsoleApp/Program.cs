32c80370e797b5d36c4538cd78be045a90556fa3
pisiaq <lukpisiak@protonmail.com>
2024-01-08 20:17:08 +0100

    Dzień 6

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,71 +1,42 @@
-﻿int number = 4566;
-string numberAsString = number.ToString();
-char[] letters = numberAsString.ToArray();
+﻿using ChallengeApp;
 
-int counter0 = 0;
-int counter1 = 0;
-int counter2 = 0;
-int counter3 = 0;
-int counter4 = 0;
-int counter5 = 0;
-int counter6 = 0;
-int counter7 = 0;
-int counter8 = 0;
-int counter9 = 0;
+Employee user1 = new Employee("Tymek", "Malesiński", 56);
+Employee user2 = new Employee("Dariusz", "Bąk", 50);
+Employee user3 = new Employee("Zosia", "Samosia", 24);
 
-foreach (char letter in letters)
+user1.AddScore(5);
+user1.AddScore(2);
+user1.AddScore(3);
+user1.AddScore(8);
+user1.AddScore(6);
+var result1 = user1.Result;
+
+user2.AddScore(4);
+user2.AddScore(2);
+user2.AddScore(5);
+user2.AddScore(6);
+user2.AddScore(9);
+var result2 = user2.Result;
+
+user3.AddScore(8);
+user3.AddScore(9);
+user3.AddScore(10);
+user3.AddScore(6);
+user3.AddScore(4);
+var result3 = user3.Result;
+
+if (result1 > result2 && result1 > result3)
 {
-    if (letter == '0')
-    {
-        counter0++;
-    }
-    else if (letter == '1')
-    {
-        counter1++;
-    }
-    else if (letter == '2')
-    {
-        counter2++;
-    }
-    else if (letter == '3')
-    {
-        counter3++;
-    }
-    else if (letter == '4')
-    {
-        counter4++;
-    }
-    else if (letter == '5')
-    {
-        counter5++;
-    }
-    else if (letter == '6')
-    {
-        counter6++;
-    }
-    else if (letter == '7')
-    {
-        counter7++;
-    }
-    else if (letter == '8')
-    {
-        counter8++;
-    }
-    else if (letter == '9')
-    {
-        counter9++;
-    }
+    Console.WriteLine($"Najwyzszy wynik ma: {user1.Name},{user1.Surname},{user1.Age}");
+    Console.WriteLine("Wynik: " + result1);
 }
-
-
-Console.WriteLine("Wynik dla liczby :" + number);
-Console.WriteLine("0 => " + counter0);
-Console.WriteLine("1 => " + counter1);
-Console.WriteLine("2 => " + counter2);
-Console.WriteLine("3 => " + counter3);
-Console.WriteLine("4 => " + counter4);
-Console.WriteLine("5 => " + counter5);
-Console.WriteLine("6 => " + counter6);
-Console.WriteLine("7 => " + counter7);
-Console.WriteLine("8 => " + counter8);
-Console.WriteLine("9 => " + counter9);
+else if (result2 > result1 && result2 > result3)
+{
+    Console.WriteLine($"Najwyzszy wynik ma: {user2.Name},{user2.Surname},{user2.Age}");
+    Console.WriteLine("Wynik: " + result2);
+}
+else if (result3 > result1 && result3 > result2)
+{
+    Console.WriteLine($"Najwyzszy wynik ma: {user3.Name},{user3.Surname},{user3.Age}");
+    Console.WriteLine("Wynik: " + result3);
+}
